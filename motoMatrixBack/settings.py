281cfd48f7fb397993acc5fd3f3347d5e53d9d91
medmoe <med.seffah@gmail.com>
2023-06-05 20:17:14 -0400

    updated settings

--- motoMatrixBack/settings.py
+++ motoMatrixBack/settings.py
@@ -157,7 +157,11 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # CORS headers configuration
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    ]
+CORS_ALLOW_CREDENTIALS = True
+
 
 # Rest framework configuration
 REST_FRAMEWORK = {
