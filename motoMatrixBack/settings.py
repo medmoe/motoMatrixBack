d95d8861c0d33310cc5644035f12d15c5935592d
medmoe <med.seffah@gmail.com>
2023-09-21 11:56:10 -0400

    sending jwt updates

--- motoMatrixBack/settings.py
+++ motoMatrixBack/settings.py
@@ -182,7 +182,7 @@ REST_FRAMEWORK = {
 
 # JWT configuration
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': datetime.timedelta(minutes=5),
+    'ACCESS_TOKEN_LIFETIME': datetime.timedelta(minutes=30),
     'REFRESH_TOKEN_LIFETIME': datetime.timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': True,
